40f8ca7e206b38bd17556bcdfb3371920f1485dd
AtlantaPatterson <atlantarpatterson@gmail.com>
2023-02-08 12:59:37 -0800

    continue schedule display

--- client/src/Test.js
+++ client/src/Test.js
@@ -4,12 +4,20 @@ function Test({onePet, petSchedule, setPetSchedule}) {
     const [monday, setMonday] = useState([])
     const [tuesday, setTuesday] = useState([])
     const [wednesday, setWednesday]= useState([])
-    const [mapWed, setMapWed] = useState()
+    const [thursday, setThursday] = useState()
+    const [friday, setFriday] = useState()
+    const [saturday, setSaturday] = useState()
+    const [sunday, setSunday] = useState()
 
-    const amObject = {AMorPM: 1, user: null}
 
-    function index(array) {
+    const amObject = {AMorPM: 1, user: 'no user'}
+    const pmObject = {AMorPM: 2, user: 'no user'}
+
+    function indexA(array) {
         return array.findIndex(x => x.AMorPM == 1 )
+    }
+    function indexP(array) {
+        return array.findIndex(x => x.AMorPM == 2 )
     }
 
     
@@ -24,69 +32,102 @@ function Test({onePet, petSchedule, setPetSchedule}) {
           }
         });
         } 
-      },[onePet]);
+      },[setPetSchedule]);
       console.log(petSchedule)
 
     useEffect(() => {
         if (petSchedule !== undefined) {
             petSchedule.forEach(schedule => {
                if (schedule.day === 1) {
-                setMonday([schedule])
+                setMonday(...monday,[schedule])
+                handleSchedule(monday)
+                setMonday(monday)
                 
             }
             if (schedule.day === 2) {
-                setTuesday([schedule])
+                setTuesday(...tuesday,[schedule])
+                handleSchedule(tuesday)
+                setTuesday(tuesday)
             } 
             if (schedule.day === 3) {
-                setWednesday([schedule])
-                noAM(wednesday)
-                // setWednesday(wednesday)
-                // setWednesday(sortArray(wednesday))
-                // setWednesday(wednesday)
-            } 
+                setWednesday(...wednesday,[schedule])
+               handleSchedule(wednesday)
+               setWednesday(wednesday)
+               
+            }
+            // if (schedule.day === 4) {
+            //     setThursday([schedule])
+            // //    handleSchedule(thursday)
+            // //    setThursday(thursday)
+               
+            // }
+            // if (schedule.day === 5) {
+            //     setFriday([schedule])
+            // //    handleSchedule(friday)
+            // //    setFriday(friday)
+               
+            // }
+            // if (schedule.day === 6) {
+            //     setSaturday([schedule])
+            // //    handleSchedule(saturday)
+            // //    setSaturday(saturday)
+               
+            // }
+            // if (schedule.day === 7) {
+            //     setSunday([schedule])
+            // //    handleSchedule(sunday)
+            // //    setSunday(sunday)
+               
+            // } 
             });
             
         } 
-      },[]);
+      },[setPetSchedule]);
 
       console.log(petSchedule)
-      console.log(index(wednesday))
+      console.log(indexA(wednesday))
+      console.log(monday)
      
+      
 
       function sortArray(array) {
         return array.sort((a, b) => a.AMorPM - b.AMorPM)
       }
 
       function noAM(array) {
-       if (index(array) == -1) {
+       if (indexA(array) == -1) {
        let newArray = array.push(amObject)
-        console.log(newArray)
+        return (newArray)
        } else {
         console.log('am exists')
        }
       }
-// console.log(wednesday.forEach(schedule => {
-//     if (!schedule.AMorPM == 1) {
-//         console.log('no AM')
-//     }
-// }))
-    //   function mapArray(array) {
-    //    return array.map((schedule) => {
-    //         return(
-    //             <p>{schedule.user}</p>
-    //         )
-    //        })
-    //   }
 
-    //   useEffect(() => {
-    //     if (wednesday.length == 2) {
-    //         setMapWed(wednesday.map((schedule) => {
-    //             return(
-    //                 <p>{schedule.user}</p>
-    //             )
-    //            }))
-    //     }
-    // })
+      function noPM(array) {
+        if (indexP(array) == -1) {
+        let newArray = array.push(pmObject)
+         return (newArray)
+        } else {
+         console.log('pm exists')
+        }
+       }
+
+      function handleSchedule(array) {
+        noAM(array)
+        noPM(array)
+        sortArray(array)
+        return array
+      }
+ 
+      function mapArray(array) {
+       return array.map((schedule) => {
+            return(
+                <p>{schedule.user}</p>
+            )
+           })
+      }
+
+  
 console.log(wednesday)
     return ( 
         <div className="calendar">
@@ -97,18 +138,19 @@ console.log(wednesday)
             <div className="days">
                 <div id='day' className="monday">
                     <p>Monday</p>
-                    <p>test</p>
-                    <p>Test</p>
+                    {mapArray(monday)}
                 </div>
                 <div id='day' className="tuesday">
                     <p>Tuesday</p>
+                    {mapArray(tuesday)}
                 </div>
                 <div id='day' className="wednesday">
                     <p>Wednesday</p>
-                    {}
+                    {mapArray(wednesday)}
                 </div>
                 <div id='day' className="thursday">
                     <p>Thursday</p>
+                    
                 </div>
                 <div id='day' className="friday">
                     <p>Friday</p>
